bf0d605e1733e5d39b68e2cef4f4d6760ef19735
keanegrech <keanegrech@gmail.com>
2025-11-30 19:20:05 +0100

    provisional setup of models

---
+++ Domain/Models/MenuItem.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Models
+{
+    public class MenuItem
+    {
+        [Key()]
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public double Price { get; set; }
+
+        [ForeignKey("ResturantFK")]
+        public virtual Resturant Resturant { get; set; }
+        public int ResturantFK { get; set; }
+        public string Status { get; set; }
+    }
+}
