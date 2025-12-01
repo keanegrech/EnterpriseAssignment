24c232790b8afc3115fd25a08181d80934befff9
keanegrech <keanegrech@gmail.com>
2025-12-01 18:51:54 +0100

    provisional setup of repos

--- DataAccess/Repositories/MenuItemRepository.cs
+++ DataAccess/Repositories/MenuItemRepository.cs
@@ -1,4 +1,5 @@
 ﻿using DataAccess.Context;
+using Domain.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,6 +14,51 @@ namespace DataAccess.Repositories
         public MenuItemRepository(ResturantMenuItemDbContext context)
         {
             _context = context;
+        }
+
+        public IQueryable<MenuItem> Get()
+        {
+            return _context.MenuItems;
+        }
+        public MenuItem Get(Guid id)
+        {
+            return _context.MenuItems.SingleOrDefault(x => x.Id == id);
+        }
+
+        public IQueryable<MenuItem> Get(string keyword)
+        {
+            return Get().Where(x => x.Title.Contains(keyword));
+        }
+
+        public void Add(MenuItem menuItem)
+        {
+            _context.MenuItems.Add(menuItem);
+            _context.SaveChanges();
+        }
+
+        public void Update(MenuItem menuItem)
+        {
+            var original = Get(menuItem.Id);
+
+            if (original != null)
+            {
+                original.Title = menuItem.Title;
+                original.Price = menuItem.Price;
+                original.Status = menuItem.Status;
+                original.ResturantFK = menuItem.ResturantFK;
+
+                _context.SaveChanges();
+            }
+        }
+
+        public void Delete(Guid id)
+        {
+            var menuItem = Get(id);
+            if (menuItem != null)
+            {
+                _context.MenuItems.Remove(menuItem);
+                _context.SaveChanges();
+            }
         }
     }
 }
--- DataAccess/Repositories/ResturantRepository.cs
+++ DataAccess/Repositories/ResturantRepository.cs
@@ -1,6 +1,8 @@
 ﻿using DataAccess.Context;
+using Domain.Models;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,6 +15,53 @@ namespace DataAccess.Repositories
         public ResturantRepository(ResturantMenuItemDbContext context)
         {
             _context = context;
+        }
+
+        public IQueryable<Resturant> Get()
+        {
+            return _context.Resturants;
+        }
+
+        public Resturant Get(int id)
+        {
+            return _context.Resturants.SingleOrDefault(x => x.Id == id);
+        }
+
+        public IQueryable<Resturant> Get(string keyword)
+        {
+            return Get().Where(x => x.Name.Contains(keyword));
+        }
+
+        public void Add(Resturant resturant)
+        {
+            _context.Resturants.Add(resturant);
+            _context.SaveChanges();
+        }
+
+        public void Update(Resturant resturant)
+        {
+            var original = Get(resturant.Id);
+
+            if (original != null)
+            {
+                original.Name = resturant.Name;
+                original.Description = resturant.Description;
+                original.OwnerEmailAddress = resturant.OwnerEmailAddress;
+                original.Address = resturant.Address;
+                original.Status = resturant.Status;
+
+                _context.SaveChanges();
+            }
+        }
+
+        public void Delete(int id)
+        {
+            var resturant = Get(id);
+            if (resturant != null)
+            {
+                _context.Resturants.Remove(resturant);
+                _context.SaveChanges();
+            }
         }
     }
 }
