1e350c8796133b36894e62812684cf76b4ee8cd5
keanegrech <keanegrech@gmail.com>
2025-12-01 21:36:53 +0100

    Showing resturants on view

--- Presentation/Controllers/ItemsController.cs
+++ Presentation/Controllers/ItemsController.cs
@@ -1,15 +1,32 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using DataAccess.Repositories;
+using Domain.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Presentation.Controllers
 {
     public class ItemsController : Controller
     {
+        private ResturantRepository _resturantRepository;
+        private MenuItemRepository _menuItemRepository;
+
+        public ItemsController(
+            ResturantRepository resturantRepository,
+            MenuItemRepository menuItemRepository
+            )
+        {
+            _resturantRepository = resturantRepository;
+            _menuItemRepository = menuItemRepository;
+        }
+
         public IActionResult Index()
         {
             string viewType = HttpContext.Request.Query["viewtype"];
             Console.WriteLine(viewType);
 
-            return View();
+            List<Resturant> resturants = _resturantRepository.Get().ToList();
+            List<MenuItem> menuItems = _menuItemRepository.Get().ToList();
+
+            return View(resturants);
         }
     }
 }
