e4d22fa9815d0ee00a6670c45579e81a214aa053
keanegrech <keanegrech@gmail.com>
2025-12-01 19:58:41 +0100

    started working on views; got query string read from controller

---
+++ Presentation/Controllers/ResturantsController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Presentation.Controllers
+{
+    public class ResturantsController : Controller
+    {
+        public IActionResult Index()
+        {
+            string viewType = HttpContext.Request.Query["viewtype"];
+            Console.WriteLine(viewType);
+
+            return View();
+        }
+    }
+}
